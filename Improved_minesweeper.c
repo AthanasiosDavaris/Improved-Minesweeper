40cbc381422d02229f9e4632af37cf928d2a7a0e
Thanos Davaris <davthn08@gmail.com>
2025-05-05 17:57:15 +0300

    Finished the main function

--- Improved_minesweeper.c
+++ Improved_minesweeper.c
@@ -21,7 +21,7 @@ int open_cell(int row, int col, int **board, int **board_state, int rows, int co
 void mark_cell(int row, int col, int **board, int **board_state);
 void cheat(int **board, int **board_state, int rows, int cols);
 void place_mines(int **board, int rows, int cols, int total_mines, int safe_row, int safe_col);
-void calculate_adj_mines(int **board, int rows, int cols);
+void calc_adj_mines(int **board, int rows, int cols);
 
 // Main Game
 int main(void)
@@ -41,9 +41,9 @@ int main(void)
     {
       printf("Invalid input. Please try again. \n");
     }
-  } while (cols < 8 || cols > 16)
+  } while (cols < 8 || cols > 16);
 
-      do
+  do
   {
     printf("Enter the number of rows (min 8, max 16): ");
     scanf("%d", &rows);
@@ -51,11 +51,10 @@ int main(void)
     {
       printf("Invalid input. Please try again. \n");
     }
-  }
-  while (rows < 8 || rows > 16)
+  } while (rows < 8 || rows > 16);
 
-    // Intitalizing the boards
-    init_boards(&board, &board_state, rows, cols);
+  // Intitalizing the boards
+  init_boards(&board, &board_state, rows, cols);
 
   // Difficulty level
   do
@@ -70,4 +69,53 @@ int main(void)
 
   // Calculates the amount of mines on the board
   total_mines = (rows * cols * (5 + 5 * difficulty)) / 100;
+
+  while (opened_cells < (rows * cols - total_mines))
+  {
+    // Print the board
+    print_board_headers(cols);
+    print_board(board, board_state, rows, cols);
+
+    // player's move
+    printf("Enter your move (e.g., open(x,y), mark(x,y), cheat(x,y), exit): ");
+    read_text(input, sizeof(input), 1);
+
+    // exit command
+    if (strncmp(input, "exit", 4) == 0)
+    {
+      exit_game(board, board_state, rows);
+    }
+
+    int x, y;
+    // open command
+    if (strncmp(input, "open", 4) == 0 && parse_cords(input, 4, &x))
+    {
+      // Initialize mines at the start
+      if (opened_cells == 0)
+      {
+        place_mines(board, rows, cols, total_mines, x - 1, y - 1);
+        calc_adj_mines(board, rows, cols);
+      }
+      opened_cells = open_cell(x - 1, y - 1, board, board_state, rows, cols);
+    }
+    // mark command
+    else if (strncmp(input, "mark", 4) == 0 && parse_cords(input, 4, &x))
+    {
+      mark_cell(x - 1, y - 1, board, board_state);
+    }
+    // cheat command
+    else if (strncmp(input, "cheat", 5) == 0 && parse_cords(input, 5, &x))
+    {
+      cheat(board, board_state, x - 1, y - 1);
+    }
+    // error in command
+    else
+    {
+      printf("Invalid input. Please use one of the following formats: open(x,y), mark(x,y), cheat(x,y), exit.\n");
+    }
+  }
+
+  // Win message
+  printf("Congratulations, you won the game!\n");
+  exit_game(board, board_state, rows);
 }
